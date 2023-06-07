29fadecf7e5dafaf76875dc98bebe3a6f81072f7
ykominami <ykominami@gmail.com>
2023-06-07 17:29:21 +0900

    fix db

--- db/migrate/20230607053905_change_nullable_to_rating_of_calibrelists.rb
+++ db/migrate/20230607053905_change_nullable_to_rating_of_calibrelists.rb
@@ -1,6 +1,5 @@
 class ChangeNullableToRatingOfCalibrelists < ActiveRecord::Migration[7.0]
   def change
-    change_column :kindlelists, :publisher, :string, null: true
-    change_column :kindlelists, :publish_date, :string, null: true
+    change_column :calibrelists, :rating, :string, null: true
   end
 end
