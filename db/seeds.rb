837fb4028e64e1dc6f6db2930e5b14fd186dd57e
ykominami <ykominami@gmail.com>
2023-07-22 08:39:01 +0900

    scaffold for abc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# 参考: https://nekorich.com/cat-names
+Abc.create(
+  [
+    {s: "アーチー", zid: 0 },
+    {s: "アーロ", zid: 1 },
+    {s: "アイヴァン", zid: 2 },
+    {s: "アイリス", zid: 3 },
+    {s: "アヴァロン", zid: 4 },
+    {s: "アクセル", zid: 5 },
+    {s: "あずき", zid: 6 },
+    {s: "アディ", zid: 7 },
+    {s: "アディソン", zid: 8 },
+    {s: "アニー", zid: 9 },
+    {s: "アビー", zid: 10 },
+    {s: "アポロ", zid: 11 },
+    {s: "アリエル", zid: 12 },
+    {s: "アリス", zid: 13 },
+    {s: "アルド", zid: 14 },
+    {s: "アルフィ", zid: 15 },
+    {s: "アレックス", zid: 16 },
+    {s: "アンバー", zid: 17 },
+    {s: "イザベラ", zid: 18 },
+    {s: "イジィ", zid: 19 },
+ 
+  ]
+)
