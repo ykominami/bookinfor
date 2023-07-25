d07e03cf6c381755fd6d033ef19d8f1ee51b6242
ykominami <ykominami@gmail.com>
2023-07-25 21:56:51 +0900

    fix layout

--- app/helpers/calibrelists_helper.rb
+++ app/helpers/calibrelists_helper.rb
@@ -5,10 +5,12 @@ module CalibrelistsHelper
         def initialize(name , calibrelists)
             @name = name
             @calibrelists = calibrelists
+            @keys = %i(title authorstags publisher languages)
+=begin
             @keys = %i(xid read_status category title title_sort tags xxid uuid isbn zid comments size series series_index
                        library_name formats timestamp pubdate publisher authors author_sort languages 
                        rating identifiers)
-        
+=end        
             @header = @keys.map{ |key| key.to_s }
             @body = @calibrelists.map{ |item| 
                 @keys.map{ |key| item[key] }        
