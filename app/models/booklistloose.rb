9628ad5560159b572cafbee93486188c379d3a9f
ykominami <ykominami@gmail.com>
2023-08-14 07:40:25 +0900

    fix book

--- app/models/booklistloose.rb
+++ app/models/booklistloose.rb
@@ -1,6 +1,8 @@
 class Booklistloose < ApplicationRecord
-    def self.ransackable_attributes(auth_object = nil)
-         ["asin", "bookstore", "category", "created_at", "id", "purchase_date", "read_status", "shape", "title", "totalID", "updated_at", "xid"]
-    end
-    
+  belongs_to :readstatus
+  belongs_to :category
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["asin", "bookstore", "category", "created_at", "id", "purchase_date", "read_status", "shape", "title", "totalID", "updated_at", "xid"]
+  end
 end
