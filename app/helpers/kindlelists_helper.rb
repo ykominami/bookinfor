bfeea4bf7dc301072ca78c8e7e146ce0c1a8f795
ykominami <ykominami@gmail.com>
2023-08-14 18:30:21 +0900

    use sub partial

--- app/helpers/kindlelists_helper.rb
+++ app/helpers/kindlelists_helper.rb
@@ -8,7 +8,7 @@ module KindlelistsHelper
       @view_context = view_context
       # @keys = %i(asin title publisher author publish_date purchase_date read_status category)
       # @keys = %i(read_status category title publisher author publish_date asin)
-      @keys = %i(read_status category title author purchase_date asin)
+      @keys = %i(id read_status category title author purchase_date asin)
       key_header = { read_status: "re" }
       @header = @keys.map { |key|
         value = key_header[key]
@@ -18,6 +18,8 @@ module KindlelistsHelper
       @body = @kindlelists.map { |item|
         array = []
         p item
+        # array << { str: (render_to_string html: kindlelist_path), attr: 100 }
+        # array << { str: "", attr: 100 }
         @keys.each_with_index { |key, index|
           # p key.class
           # if key == :read_status
@@ -29,20 +31,41 @@ module KindlelistsHelper
             # item[key] = key.size
           end
 
+          case key
+          when :id
+            array << { str: item[key], attr: 20 }
+          when :read_status
+            array << { str: item.readstatus.name, attr: 60 }
+          when :category
+            array << { str: item.category.name, attr: 100 }
+          when :title
+            array << { str: item[key], attr: 400 }
+          when :author
+            array << { str: item[key], attr: 200 }
+          when :purchase_date
+            array << { str: item[key], attr: 100 }
+          else
+            # :asin
+            array << { str: item[key], attr: "" }
+          end
+=begin
           case index
           when 0
-            array << { str: item.readstatus.name, attr: 20 }
+            array << { str: item[key], attr: 20 }
           when 1
-            array << { str: item.category.name, attr: 100 }
+            array << { str: item.readstatus.name, attr: 20 }
           when 2
-            array << { str: item[key], attr: 400 }
+            array << { str: item.category.name, attr: 100 }
           when 3
-            array << { str: item[key], attr: 200 }
+            array << { str: item[key], attr: 400 }
           when 4
+            array << { str: item[key], attr: 200 }
+          when 5
             array << { str: item[key], attr: 100 }
           else
             array << { str: item[key], attr: "" }
           end
+=end
         }
         array
       }
