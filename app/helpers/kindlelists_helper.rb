22c6a627bd57b3bc9a77c65065e12b82aef1487d
ykominami <ykominami@gmail.com>
2023-08-14 09:50:07 +0900

    use kaminari and tbl component adn form on turbo_frame and sub partial, hide status in index action of Reading, ignore *-2.hyml.rtn

--- app/helpers/kindlelists_helper.rb
+++ app/helpers/kindlelists_helper.rb
@@ -10,7 +10,8 @@ module KindlelistsHelper
       @view_context = view_context
       # @keys = %i(asin title publisher author publish_date purchase_date read_status category)
       # @keys = %i(read_status category title publisher author publish_date asin)
-      @keys = %i[read_status category title]
+      @keys = %i(id read_status category title author purchase_date asin)
+      # @keys = %i[read_status category title]
       key_header = { read_status: "re" }
       @header = @keys.map do |key|
         value = key_header[key]
@@ -19,25 +20,73 @@ module KindlelistsHelper
       end
       @body = @kindlelists.map do |item|
         array = []
-        @logger.debug item
-        @keys.each_with_index do |key, index|
-          item[key] = 0 if key == :read_status && item[key].nil?
+        p item
+        # array << { str: (render_to_string html: kindlelist_path), attr: 100 }
+        # array << { str: "", attr: 100 }
+        @keys.each_with_index { |key, index|
+          # p key.class
+          # if key == :read_status
+          if key == :read_status
+            # item[key] = render_to_string LinkButttonComponent.new(label: "finish", url: "/abc")
+            item[key] = 0 unless item[key]
+            # item[key] = LinkButtonComponent.new(label: "#{key}", url: "/abc").render_in(@view_context)
+          else
+            # item[key] = key.size
+          end
 
+          case key
+          when :id
+            array << { str: item[key], attr: 20 }
+          when :read_status
+            array << { str: item.readstatus.name, attr: 60 }
+          when :category
+            array << { str: item.category.name, attr: 100 }
+          when :title
+            array << { str: item[key], attr: 400 }
+          when :author
+            array << { str: item[key], attr: 200 }
+          when :purchase_date
+            array << { str: item[key], attr: 100 }
+          else
+            # :asin
+            array << { str: item[key], attr: "" }
+          end
+=begin
           case index
           when 0
-            array << { str: item.readstatus.name, attr: 20 }
+            array << { str: item[key], attr: 20 }
           when 1
-            array << { str: item.category.name, attr: 100 }
+            array << { str: item.readstatus.name, attr: 20 }
           when 2
-            array << { str: item[key], attr: 400 }
+            array << { str: item.category.name, attr: 100 }
           when 3
+<<<<<<< HEAD
+            array << { str: item[key], attr: 400 }
+          when 4
+||||||| parent of 8ff26ea (use form on turbo_frame)
             array << { str: item[key], attr: 100 }
           when 4
+=======
+>>>>>>> 8ff26ea (use form on turbo_frame)
             array << { str: item[key], attr: 200 }
+<<<<<<< HEAD
+          when 5
+            array << { str: item[key], attr: 100 }
+            # array << { str: item[key], attr: 200 }
+          when 5
+            array << { str: item[key], attr: 100 }
+            # array << { str: item[key], attr: 200 }
+            # array << { str: item[key], attr: 400 }
+||||||| parent of 8ff26ea (use form on turbo_frame)
+=======
+          when 4
+            array << { str: item[key], attr: 100 }
+>>>>>>> 8ff26ea (use form on turbo_frame)
           else
             array << { str: item[key], attr: "" }
           end
-        end
+=end
+        }
         array
       end
       @logger.debug @body
