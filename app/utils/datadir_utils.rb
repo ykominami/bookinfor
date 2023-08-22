50eb8ac9ac42b675b44ed62c16308c249ee82811
ykominami <ykominami@gmail.com>
2023-08-22 20:45:49 +0900

    export db to json file

--- app/utils/datadir_utils.rb
+++ app/utils/datadir_utils.rb
@@ -1,11 +1,12 @@
 require "pathname"
 
 class DatadirUtils
-  attr_reader :output_pn
+  attr_reader :output_pn, :export_pn
 
   def initialize
-    @output_dir = ConfigUtils.output_dir
-    @output_pn = Pathname.new(@output_dir)
+    @output_pn = ConfigUtils.output_dir_pn
+    @export_pn = ConfigUtils.output_export_dir_pn
     @output_pn.mkdir() unless @output_pn.exist?
+    @export_pn.mkdir() unless @export_pn.exist?
   end
 end
