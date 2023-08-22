74b510526baf05996d0455df3bcdd64b1c504ddf
ykominami <ykominami@gmail.com>
2023-08-22 20:45:49 +0900

    export db to json file

---
+++ app/exporter/all_exporter.rb
@@ -0,0 +1,55 @@
+class AllExporter
+  def initialize(cmd, search_file_pn = nil, local_file_pn = nil)
+    config_pn = ConfigUtils.config_pn
+    aux_dbtbl_pn = ConfigUtils.aux_dbtbl_pn
+    p aux_dbtbl_pn
+    # state_pn = ConfigUtils.state_pn
+
+    @datadir = DatadirUtils.new()
+
+    # @search_file_pn = search_file_pn if search_file_pn && search_file_pn.exist?
+    # @local_file_pn = local_file_pn if local_file_pn && local_file_pn.exist?
+
+    # @state = JsonUtils.parse(state_pn)
+    ###############
+    export_or_import(cmd, aux_dbtbl_pn)
+    export_or_import(cmd, config_pn)
+  end
+
+  def export_or_import(cmd, json_pn)
+    configx = ConfigUtils.get_configx(json_pn)
+    keys = configx.get_keys()
+    keys.map { |key|
+      klass = configx.get_class(key)
+      output_pn = make_file_pn(@datadir.export_pn, key)
+      puts "output_pn=#{output_pn.to_s}"
+      puts klass
+      if cmd == :export
+        export(klass, output_pn)
+      else
+        import(klass, output_pn)
+      end
+    }
+  end
+
+  def make_file_pn(dir_pn, key)
+    file_path = "#{key}.json"
+    dir_pn + file_path
+  end
+
+  def export(klass, output_pn)
+    dump = JSON.generate(klass.all.to_a.map { |item|
+      item.attributes
+    })
+    File.write(output_pn, dump)
+  end
+
+  def import(klass, output_pn)
+    content = File.read(output_pn)
+    obj = JSON.parse(content)
+    p output_pn
+    klass.insert_all(obj)
+    # item.attributes
+
+  end
+end
--- app/utils/config_utils.rb
+++ app/utils/config_utils.rb
@@ -1,14 +1,77 @@
 class ConfigUtils
+  class Configx
+    def initialize(path_pn)
+      @path_pn = path_pn
+      @json = JsonUtils.parse(@path_pn)
+      @obj = @json["xkeys"]
+    end
+
+    def get_keys
+      @json["keys"]
+    end
+
+    def get_class(key)
+      @obj[key]["ac_klass"].constantize
+    end
+  end
+
   attr_reader :output_pn
 
   @src_url = "https://a.northern-cross.net/gas2/a.php"
   @html_filename = "b0.html"
   @output_dir = "data"
-  @config_dir_pn = Rails.root + "config" + "importers"
-  @config_pn = @config_dir_pn + "config.json"
+  # @config_dir_pn = Rails.root + "config" + "importers"
+
+  @config_dir_pn = Rails.root + "config"
+  @importer_config_dir_pn = @config_dir_pn + "importers"
+  @exporter_config_dir_pn = @config_dir_pn + "exporters"
+
+  @output_dir_pn = Rails.root + @output_dir
+  @export_output_dir_pn = @output_dir_pn + "export"
+
+  @config_pn = @importer_config_dir_pn + "config.json"
+
+  @aux_dbtbl_pn = @importer_config_dir_pn + "aux_dbtbl.json"
+
   @datalist_json_filename = "datalist.json"
-  @state_pn = @config_dir_pn + "state.json"
+  @state_pn = @importer_config_dir_pn + "state.json"
   @use_import_date = false
+
+  @export_from_db_dir = "export"
+  @output_export_dir_pn = @output_dir_pn + @export_from_db_dir
+  @import_from_exported_data = ""
+
+  def self.output_export_dir_pn
+    @output_export_dir_pn
+  end
+
+  def self.get_configx(path)
+    Configx.new(path)
+  end
+
+  def self.output_dir_pn
+    @output_dir_pn
+  end
+
+  def self.export_output_dir_pn
+    @export_output_dir_pn
+  end
+
+  def self.config_dir_pn
+    @config_dir_pn
+  end
+
+  def self.aux_dbtbl_pn
+    @aux_dbtbl_pn
+  end
+
+  def self.importer_config_dir_pn
+    @importer_config_dir_pn
+  end
+
+  def self.exporter_config_dir_pn
+    @exporter_config_dir_pn
+  end
 
   def self.dl_src_url
     @src_url
@@ -20,10 +83,6 @@ class ConfigUtils
 
   def self.output_dir
     @output_dir
-  end
-
-  def self.config_dir_pn
-    @config_dir_pn
   end
 
   def self.config_pn
