a1ece2000cd1bbf9aec97404ee7d2ff42d619f08
ykominami <ykominami@gmail.com>
2024-02-15 13:29:20 +0900

    fxi

--- app/models/readinglist.rb
+++ app/models/readinglist.rb
@@ -2,7 +2,10 @@ class Readinglist < ApplicationRecord
   belongs_to :readstatus
   belongs_to :shape
 
-  default_scope { order("date DESC") }
+  # default_scope { order(:date :desc) }
+  # default_scope { order(:date :desc) }
+  # default_scope { order(:date :desc, :title) }
+  default_scope -> { order(date: :desc).order(:title) }
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "date", "id", "isbn", "readstatus_id", "register_date", "shape", "status", "title", "updated_at", "shape_id"]
