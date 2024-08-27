3269bba227bcdce3822d4dffa54072c58be7f996
ykominami <ykominami@gmail.com>
2024-08-27 16:20:12 +0900

    fix migrate files

--- db/migrate/20230813120528_add_categoriy_id_to_booklisttights.rb
+++
@@ -1,6 +0,0 @@
-class AddCategoriyIdToBooklisttights < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :booklisttights, :category, foreign_key: true
-    change_column :booklisttights, :category_id, :integer, null: false
-  end
-end
