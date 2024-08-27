0057c0512282324a9c54ee392100b4d725896034
ykominami <ykominami@gmail.com>
2024-08-27 16:20:12 +0900

    fix migrate files

--- db/migrate/20230813141455_add_category_id_to_readinglists.rb
+++
@@ -1,6 +0,0 @@
-class AddCategoryIdToReadinglists < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :readinglists, :category, foreign_key: true
-    change_column :readinglists, :category_id, :integer, null: false
-  end
-end
--- db/migrate/20230824231311_delete_bookstore_from_booklist.rb
+++
@@ -1,5 +0,0 @@
-class DeleteBookstoreFromBooklist < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :booklists, :bookstore
-  end
-end
